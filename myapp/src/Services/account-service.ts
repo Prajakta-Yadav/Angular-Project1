b1b2a22da5d0d74b69d5f38747b65011a73ce7a9
Prajakta-Yadav <prajaktayadav2883@gmail.com>
2026-05-15 03:59:50 +0530

    Update UI

--- myapp/src/Services/account-service.ts
+++ myapp/src/Services/account-service.ts
@@ -10,6 +10,7 @@ export class AccountService {
   constructor(private http: HttpClient){}
 
   onLogin(obj: any): Observable <any> {
+    
     return this.http.post('http://cispl.runasp.net/api/Auth/login', obj); 
 
   }
--- myapp/src/SmartComponent/admin-login/admin-login.ts
+++ myapp/src/SmartComponent/admin-login/admin-login.ts
@@ -1,39 +1,33 @@
 import { Component, inject } from '@angular/core';
 import { AccountService } from '../../Services/account-service';
 import { Router, RouterOutlet, RouterLink } from '@angular/router';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControlName, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-login',
-  imports: [FormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterOutlet],
   templateUrl: './admin-login.html',
   styleUrl: './admin-login.css',
 })
+
 export class AdminLogin {
-
-  loginObj : any = {
-    username: '',
-    password : '' 
-  }; 
-
-  /**
-   * private fb = inject(FormBuilder); 
+  
+  private fb = inject(FormBuilder); 
 
 
   form = this.fb.group({
     username: ['', Validators.required],
     password: ['', Validators.required],
   }); 
-   */
-
+   
 
   constructor(private accountService: AccountService, private route: Router){}
 
   onLogin(){
     debugger
 
-    this.accountService.onLogin(this.loginObj).subscribe((res: any) => {
+    this.accountService.onLogin(this.form.value).subscribe((res: any) => {
       debugger
       console.log('res', res)
       localStorage.setItem('token', res.token)
@@ -42,8 +36,5 @@ export class AdminLogin {
     })
 
   }
-
-
-
   
 }
